040e41dcacf920065e7854530d2e8fde2ac253b6
SaraSquibb18 <sara.squibb@pcc.edu>
2018-10-02 23:51:47 -0700

    Create HelloWorld class

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+
+/**
+ * Say Hello.
+ *
+ * @author Sara Squibb
+ * @version 2018.10.02
+ */
+public class HelloWorld
+{
+    /**
+     * Say hello to the world
+     */
+    public void sayHello()
+    {
+        System.out.println("Hello, World!");
+    }
+    
+}
