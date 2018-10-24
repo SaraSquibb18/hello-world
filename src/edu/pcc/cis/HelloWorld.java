fd1c92e63ad5a90d270a40365243232866247f3c
SaraSquibb18 <sara.squibb@pcc.edu>
2018-10-23 21:44:47 -0700

    added main method that prints hello world

---
+++ src/edu/pcc/cis/HelloWorld.java
@@ -0,0 +1,11 @@
+package edu.pcc.cis;
+
+/**
+ * created by Sara Squibb 10/23/18.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
